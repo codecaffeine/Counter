475f8f5c28c9e4936155e47227a7847d7f17b553
codecaffeine <matt@codecaffeine.com>
2021-08-28 15:26:36 -0400

    Initial Commit

---
+++ Shared/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Shared
+//
+//  Created by Matt Thomas on 8/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
